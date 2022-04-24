a56a54709b4de81df0474e975428ecab7c72b7ce
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2022-04-24 21:34:43 +0300

    ee

--- main1.py
+++ main1.py
@@ -1,4 +1,5 @@
-def __init__(self, radius: float, height: float) -> None:
+def __init__(self, radius: float, height: float, d) -> None:
     """The function creates attributes of the objects of this class"""
     self.radius, self.height = radius, height
     self.slant_height = self.radius ** 2 + self.height ** 2
+    self.d = d
