7495aa8290164861f12ef9dcc7bb0f06f50995de
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2022-05-17 22:54:12 +0300

    Delete main1.py

--- main1.py
+++
@@ -1,9 +0,0 @@
-import math
-import random
-
-
-def __init__(self, radius: float, height: float, d) -> None:
-    """The function creates attributes of the objects of this class"""
-    self.radius, self.height = radius, height
-    self.slant_height = self.radius ** 2 + self.height ** 2
-    self.d = d * 4
